89a560a6e8d40481a46e8e6391db3d231642e68f
Leonardo Lujan <LeonardoLujan72@gmail.com>
2023-07-24 22:44:34 -0400

    Added lessons for Port, adding nav bar addition

---
+++ src/app/Login/select/portuguese/lesson3/page.tsx
@@ -0,0 +1,13 @@
+import PortL3NewIndex from 'src/app/components/learning/portuguesewords/PortL3/PortL3NewIndex.tsx'
+
+export default function PortLesson3() {
+    return(
+        <div class="text-black justify-center p-8">
+            
+            
+                <PortL3NewIndex/>
+        
+        
+        </div>
+    )
+}
